636c05f4efad266b12b4795dc75b1c281991ab33
Taras <taras.malchenko@gmail.com>
2023-03-16 22:01:44 +0200

    adaptive

--- src/components/CotologueItemCard.vue
+++ src/components/CotologueItemCard.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <router-link :to="`/detail/${catologueWine.id}`">
-      <div class=" flex flex-col items-center justify-center mb-3 bg-[#dddddd] h-[300px] w-[250px] p-2 drop-shadow-2xl rounded-md hover:scale-105 sm:h-[500px] xs:h-[350px] sm:w-[250px] xs:w-[250px]">
+      <div class=" flex flex-col items-center justify-center mb-3 bg-[#dddddd] h-[300px] w-[250px] p-2 drop-shadow-2xl rounded-md hover:scale-105 sm:h-[500px] xs:h-[350px] sm:w-[175px] xs:w-[175px]">
         <img
           :src="(catologueWine.img)"
           :alt="catologueWine.name"
-          class="w-40 h-40 max-w-fit sm:h-3/4 xs:h-3/5 sm:w-[140px] xs:w-[140px]"
+          class="w-40 h-40 max-w-fit sm:h-3/4 xs:h-3/5 sm:w-[75px] xs:w-[75px]"
         >
         <p class="text-md max-w-[150px] text-center mt-2">
           {{ catologueWine.name }}
