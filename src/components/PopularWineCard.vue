bd6546eba5b5143c49eb3c543f6091605aaf85b8
Taras <taras.malchenko@gmail.com>
2023-03-16 22:06:26 +0200

    adaptive

--- src/components/PopularWineCard.vue
+++ src/components/PopularWineCard.vue
@@ -1,10 +1,10 @@
 <template>
   <router-link :to="`/detail/${popularWines.id}`">
-    <div class="flex flex-col items-center justify-center bg-[#dddddd] h-[270px] p-2 drop-shadow-2xl rounded-md hover:scale-105 sm:h-[400px] xs:h-[350px] sm:w-[175px] xs:w-[175px]">
+    <div class="flex flex-col items-center justify-center bg-[#dddddd] h-[270px] p-2 drop-shadow-2xl rounded-md hover:scale-105 sm:h-[400px] xs:h-[350px] sm:w-[250px] xs:w-[175px]">
       <img
         :src="popularWines.img"
         :alt="popularWines.name"
-        class="max-w-fit w-40 h-40 sm:h-3/4 xs:h-3/5 sm:w-[75px] xs:w-[75px]"
+        class="max-w-fit w-40 h-40 sm:h-3/4 xs:h-3/5 sm:w-[150px] xs:w-[75px]"
       >
       <p class="text-md max-w-[150px] text-center">
         {{ popularWines.name }}
